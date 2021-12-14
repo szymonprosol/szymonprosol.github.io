26359bf5b2b92dda19af9b315bae0619bc177515
Szymon Rosół <szymon.patryk.rosol@gmail.com>
2021-12-14 18:28:01 +0100

    trelloApiRoot has been changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://kodilla-tasks1-mr.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://trello.com/1/members/szymonroso/boards';
+   const trelloApiRoot = 'https://kodilla-tasks1-mr.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
