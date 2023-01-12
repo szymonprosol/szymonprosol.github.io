ff8cf5ec4f0b08cab3f2b13d65d37a1ea25ecfe9
Szymon Rosół <81100512+szymonprosol@users.noreply.github.com>
2023-01-12 20:17:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    const apiRoot = 'https://programowanie-zaliczenie.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
+   const trelloApiRoot = 'https://programowanie-zaliczenie.herokuapp.com/v1/task/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
